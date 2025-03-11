26b0a63425e2217fa3d0efcc55d9b1940c1cb2cb
psyko944 <mehdi.kherboucheidf@gmail.com>
2025-03-11 17:09:09 +0100

    ca bounce

--- module04/ex02/main.c
+++ module04/ex02/main.c
@@ -1,38 +1,60 @@
 #include "atmega328p_common.h"
 
 volatile int count = 0;
+volatile uint8_t debounce_time = 0;
 ISR(INT0_vect)
 {
-	if (!PIND & (1 << PD2))
+	if (!(PIND & (1 << PD2)) && debounce_time == 0)
 	{
-		PORTB = count;
-		if (PORTB == 0x08)  // if we are at pb3, skip to pb4. pb3 begin at 0x08 = 0000 1000, we move at 0x10 = 0001 0000
-				PORTB = 0x10; // skip to the next led
-		count = (count + 1) % 23;
-		while (!(PIND & (1 << PD2))) // delay for avoid bouncing
-				_delay_ms(50);
+		if (count < 23)
+		{
+			count++;
+			PORTB = count;
+		}
+		// _delay_ms(50);
+		debounce_time = 50;
 	}
-
 }
 
 ISR(PCINT2_vect)
 {
-	if (!PIND & (1 << PD4))
-		count--;
+	if (!(PIND & (1 << PD4)) && debounce_time == 0)
+	{
+		PORTB |= D1;
+		if (count > 0)
+		{
+			count--;
+			PORTB = count;
+		}	
+		_delay_ms(50);
+		debounce_time = 50;
+	}
 }
 
+ISR(TIMER0_COMPA_vect)
+{
+	if (debounce_time > 0)
+		debounce_time--;
+}
 int main()
 {
-	DDRB = (1 << PB0);
-	DDRD &= ~(1 << PD2) | (1 << PD4);
+	DDRB = D1 | D2 | D3 | D4;
+	DDRD &= ~((1 << PD2) | (1 << PD4));
 	PORTD |= (1 << PD2) | (1 << PD4);
 	EICRA |= (1 << ISC01);
 	EICRA &= ~(1 << ISC00);
 	EIMSK |= (1 << INT0);
 	PCICR |= (1 << PCIE2);
 	PCMSK2 |= (1 << PCINT20);
+
+
+// Timer setup
+    TCCR0A = (1 << WGM01); // CTC mode
+    OCR0A = 156;           // Adjust for desired debounce interval
+    TIMSK0 = (1 << OCIE0A); // Enable compare match interrupt
+    TCCR0B = (1 << CS02);  // Prescaler 256
+
 	sei();
-	// SREG & (1 << 7)
 	while (1)
 	{
 	}	
