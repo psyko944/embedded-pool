78880b1e8c28198c902cff64e6ebf6676b3987ee
Mehdi Kherbouche <mekherbo@made-f0Dr9s15.clusters.42paris.fr>
2025-03-12 21:01:03 +0100

    add module 05

---
+++ module05/ex00/main.c
@@ -0,0 +1,20 @@
+#include "atmega328p_common.h"
+
+
+
+int main()
+{
+	ADMUX = (1 << REFS0) | (1 << ADLAR); // use AVCC, set 8 bits
+  	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Enable ADC, prescaler 128
+	  
+	  init_uart();
+	  while (1)
+	  {
+		ADCSRA |= (1 << ADSC);
+		while (ADCSRA & (1 << ADSC));
+		uart_print_hexa(ADCH);
+		uart_printstr("\r\n");
+		_delay_ms(20);
+	}	
+	return 0;  // return 0 if the program is successfull
+}
