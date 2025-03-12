9b4c514d35f2fd7c876dcf2de3588bd2cf360ac1
psyko944 <mehdi.kherboucheidf@gmail.com>
2025-03-13 00:17:53 +0100

    begin modile 05

--- module05/ex02/main.c
+++ module05/ex02/main.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-	ADMUX = (1 << REFS0) | (1 << ADLAR); // use AVCC, set 8 bits
+	ADMUX = (1 << REFS0); // use AVCC, set 8 bits
   	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Enable ADC, prescaler 128
 	  
 	init_uart();
@@ -14,21 +14,21 @@ int main()
 		ADMUX = (ADMUX & 0xF8) | 0; 
 		ADCSRA |= (1 << ADSC);
 		while (ADCSRA & (1 << ADSC));
-		uart_print_hexa(ADCH);
+		uart_print_number(ADC);
 		uart_printstr(", ");
 
 		// Read LDR			
 		ADMUX = (ADMUX & 0xF8) | 1; 
 		ADCSRA |= (1 << ADSC);
 		while (ADCSRA & (1 << ADSC));
-		uart_print_hexa(ADCH); 
+		uart_print_number(ADC); 
 		uart_printstr(", ");
 
 		// Read NTC
 		ADMUX = (ADMUX & 0xF8) | 2; 
 		ADCSRA |= (1 << ADSC);
 		while (ADCSRA & (1 << ADSC));
-		uart_print_hexa(ADCH);
+		uart_print_number(ADC);
 
 		uart_printstr("\r\n");
 		_delay_ms(20);
